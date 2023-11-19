f0efc3b070ede539dd28956d22d82f515e01e715
ConceptualCode <soronnadianthony@gmail.com>
2023-11-19 13:57:57 +0100

    Added text to image function

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import requests
 import streamlit as st
 from PIL import Image
+#from transformers import StableDiffusionPipeline
+import torch
 
 def load_image_from_url(url):
     try:
@@ -10,3 +12,11 @@ def load_image_from_url(url):
     except requests.RequestException as e:
         st.error(f"Error loading image from URL: {e}")
         return None
+
+def generate_image_from_text(model, prompt, device="cuda"):
+    with torch.no_grad():
+        generated_images = model(prompt)["images"]
+        
+    generated_image = generated_images[0]
+
+    return generated_image
